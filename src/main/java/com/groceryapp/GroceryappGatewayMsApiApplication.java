00d37e200e0d6f271a77836ec8842589ae4ab567
sunayanapandra <pandrasunayana@gmail.com>
2022-05-05 18:09:35 +0530

    added src pom.xml

---
+++ src/main/java/com/groceryapp/GroceryappGatewayMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.groceryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryappGatewayMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryappGatewayMsApiApplication.class, args);
+		System.out.println("gateway working.....");
+	}
+
+}
